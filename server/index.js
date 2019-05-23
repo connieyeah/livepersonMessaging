ef04546e6dc4fbd6244ae28f0ba0cdc8b3a86611
Connie Ea <connieea@Connies-MacBook-Pro.local>
2019-05-23 14:06:42 -0700

    remove console.log in server code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const path = require('path');
 const WebSocket = require('ws');
 const request = require('request');
 
@@ -31,21 +30,20 @@ request.post('https://va.idp.liveperson.net/api/account/40912224/signup', (err, 
     }});
 
     ws.onopen = function (socket) {
-      console.log('socket connection opened properly');
+      console.log('socket connection opened');
       const newConversation = { "kind":"req","id":1,"type":"cm.ConsumerRequestConversation" }
       ws.send(JSON.stringify(newConversation));
-    
     
       console.log('Request for new conversation sent');
     };
     
     ws.onmessage = function (evt) {
-      console.log("Message received = " + evt.data);
+      // console.log("Message received = " + evt.data);
       const cId = JSON.parse(evt.data).body.conversationId
-      console.log('conversation id: ', cId)
+      // console.log('conversation id: ', cId)
       const message = getPublishMessage(cId)
       if (cId) {
-        console.log(`what is meesage: `, message)
+        // console.log(`what is message: `, message)
         ws.send(JSON.stringify(message))
         ws.close()
       }
@@ -57,51 +55,6 @@ request.post('https://va.idp.liveperson.net/api/account/40912224/signup', (err, 
       console.log("Connection closed...");
     };
 });
-
-
-// const ws = new WebSocket ('wss://va.msg.liveperson.net/ws_api/account/40912224/messaging/consumer?v=3', {
-//     headers : {
-//       Authorization: "jwt " + LIVEPERSON_JWT
-//     }});
-
-
-
-// ws.onopen = function (socket) {
-//   console.log('socket connection opened properly');
-//   const newConversation = { "kind":"req","id":1,"type":"cm.ConsumerRequestConversation" }
-//   ws.send(JSON.stringify(newConversation));
-
-
-//   console.log('Request for new conversation sent');
-// };
-
-// ws.onmessage = function (evt) {
-//   console.log("Message received = " + evt.data);
-//   const cId = JSON.parse(evt.data).body.conversationId
-//   console.log('conversation id: ', cId)
-//   const message = {
-//     "kind": "req",
-//     "id": 2,
-//     "type": "ms.PublishEvent",
-//     "body": { 
-//       "dialogId": cId,
-//       "event": {
-//         "type": "ContentEvent",
-//         "contentType": "text/plain",
-//         "message": "HELLO TESTING TESTING"
-//       }
-//     }
-//   }
-//   console.log(`what is meesage: `, message)
-//   ws.send(JSON.stringify(message))
-//   ws.close()
-// };
-
-// ws.onclose = function () {
-//   // websocket is closed.
-//   console.log("Connection closed...");
-// };
-
 
 const port = process.env.PORT || 3000;
 http.listen(port, (err) => {
